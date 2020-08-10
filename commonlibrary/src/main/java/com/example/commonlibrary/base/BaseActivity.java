b3bc0d3d8d4648003dd9ad040b161546fe1e3e9f
songyuliang <syliang126@126.com>
2020-08-10 15:41:30 +0800

    修改gradle版本解决 Attempt to use @BindView for an already bound ID 0 问题

--- commonlibrary/src/main/java/com/example/commonlibrary/base/BaseActivity.java
+++ commonlibrary/src/main/java/com/example/commonlibrary/base/BaseActivity.java
@@ -7,11 +7,17 @@ import androidx.annotation.Nullable;
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public abstract class BaseActivity extends RxAppCompatActivity {
+    private Unbinder unbinder;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(initLayout());
+        unbinder = ButterKnife.bind(this);
         initView();
         initData();
     }
@@ -55,4 +61,9 @@ public abstract class BaseActivity extends RxAppCompatActivity {
     }
 
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
 }
--- main/src/main/java/com/example/main/LoginActivity.java
+++ main/src/main/java/com/example/main/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.example.main;
 
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.example.commonlibrary.base.BaseActivity;
+import com.example.commonlibrary.listener.OnSingleClickListener;
 import com.example.main.adapter.SplashAdapter;
 import com.example.main.view.ScollLinearLayoutManager;
 import butterknife.BindView;
@@ -11,10 +17,12 @@ import butterknife.BindView;
 
 @Route(path = "/main/login")
 public class LoginActivity extends BaseActivity {
-
-
     @BindView(R2.id.mRecyclerView)
     RecyclerView mRecyclerView;
+    @BindView(R2.id.tv_regist)
+    TextView tvRegist;
+    @BindView(R2.id.tv_login)
+    TextView tvLogin;
 
     @Override
     protected int initLayout() {
@@ -29,6 +37,13 @@ public class LoginActivity extends BaseActivity {
 
         //smoothScrollToPosition滚动到某个位置（有滚动效果）
         mRecyclerView.smoothScrollToPosition(Integer.MAX_VALUE / 2);
+        tvLogin.setOnClickListener(new OnSingleClickListener() {
+            @Override
+            public void onSingleClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
