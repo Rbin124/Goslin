03cd11d459b8ab48a2bdc18b939f9a4dabcf5211
songyuliang <syliang126@126.com>
2020-08-18 17:19:13 +0800

    添加log日志

--- app/src/main/java/com/example/goslin/MyApplication.java
+++ app/src/main/java/com/example/goslin/MyApplication.java
@@ -4,17 +4,20 @@ import androidx.multidex.MultiDex;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.example.commonlibrary.base.BaseApplication;
+import com.example.commonlibrary.utils.LogUtil;
 
 public class MyApplication extends BaseApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
-        //       if (BuildConfig.DEBUG) {
-        //一定要在ARouter.init之前调用openDebug
-        ARouter.openDebug();
-        ARouter.openLog();
+        if (BuildConfig.DEBUG) {
+            //一定要在ARouter.init之前调用openDebug
+            ARouter.openDebug();
+            ARouter.openLog();
+        }
         ARouter.init(this);
+        LogUtil.init(BuildConfig.LOG_DEBUG);
         MultiDex.install(this);
     }
 }
--- commonlibrary/src/main/java/com/example/commonlibrary/net/manager/RetrofitManager.java
+++ commonlibrary/src/main/java/com/example/commonlibrary/net/manager/RetrofitManager.java
@@ -9,35 +9,13 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitManager {
-    private Retrofit mRetrofit;
-
-//    private static class InstanceHelper {
-//        static RetrofitManager instance = new RetrofitManager();
-//    }
-//
-//    public static RetrofitManager getInstance() {
-//        return InstanceHelper.instance;
-//    }
-//
-//    private RetrofitManager() {
-//        mRetrofit = new Retrofit.Builder()
-//                //设置baseUrl
-//                .baseUrl("https://shop.yuelvhui.com")
-//                //设置OkHttpClient对象
-//                .client(createOkhttpClient())
-//                .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // 支持RxJava
-//                .addConverterFactory(GsonConverterFactory.create()) // 支持Gson解析
-//                .addConverterFactory(ScalarsConverterFactory.create())//支持字符串
-//                .build();
-//    }
 
     /*
     * 创建OkHttpClient对象。Retrofit底层基于OkHttpClient进行网络请求。
     * */
-    private static OkHttpClient createOkhttpClient() {
+    private static OkHttpClient createOkHttpClient() {
         HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new HttpLogger());
         httpLoggingInterceptor.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
         return new OkHttpClient.Builder()
@@ -47,16 +25,11 @@ public class RetrofitManager {
                 .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(30, TimeUnit.SECONDS)
                 //添加日志过滤器
-                .addInterceptor(httpLoggingInterceptor)
+                .addInterceptor(new BaseInterceptor())
                 .addNetworkInterceptor(httpLoggingInterceptor)
                 //添加BaseInterceptor过滤器
-                .addInterceptor(new BaseInterceptor())
                 .build();
     }
-
-//    public <T> T createApi(final Class<T> service) {
-//        return mRetrofit.create(service);
-//    }
 
     /**
      * 获取Service
@@ -69,7 +42,7 @@ public class RetrofitManager {
     public static <T> T create(Class<T> clazz) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://shop.yuelvhui.com")
-                .client(createOkhttpClient())
+                .client(createOkHttpClient())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // 支持RxJava
                 .addConverterFactory(GsonConverterFactory.create()) // 支持Gson解析
                 .build();
